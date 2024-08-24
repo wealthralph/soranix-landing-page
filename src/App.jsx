6f27afe0a4aa54dbabe7dc016ceae1e767221a1b
catalyst <wealthralph@gmail.com>
2024-08-24 15:41:17 +0100

    Init> Intialized the soranix landing page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createTheme, MantineProvider } from "@mantine/core";
+import Navbar from "./component/navabar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import WebLayout from "./component/layout/webLayout";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
-function App() {
-  const [count, setCount] = useState(0)
+const theme = createTheme({
+  primaryColor: "cyan",
+});
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <MantineProvider
+        defaultColorScheme="auto"
+        theme={theme}
+        classNamesPrefix="web"
+      >
+        <Routes>
+          <Route path="/">
+            <Route element={<WebLayout />}>
+              <Route index element={<Home />} />
+            </Route>
+          </Route>
+        </Routes>
+      </MantineProvider>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ src/component/layout/webLayout.jsx
@@ -0,0 +1,35 @@
+import { AppShell, Burger, Group, Stack, UnstyledButton } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import Navbar from "../navabar/Navbar";
+import { Outlet } from "react-router-dom";
+
+function WebLayout() {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { desktop: true, mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header >
+        <Stack h={'100%'} >
+
+        <Navbar />
+        </Stack>
+      </AppShell.Header>
+
+      <AppShell.Navbar py="md" px={4}></AppShell.Navbar>
+
+      <AppShell.Main>
+        <Outlet/>
+      </AppShell.Main>
+    </AppShell>
+  );
+}
+
+export default WebLayout;
