ccd71fa3f921b8d4c8880d3f56242d471dade6f5
FK <mac@Macs-MacBook.local>
2024-09-24 22:45:10 +0100

    updted the networth blurry graphics

--- src/pages/Home/networthSection/NetworthSection.jsx
+++ src/pages/Home/networthSection/NetworthSection.jsx
@@ -1,97 +1,99 @@
-import { ActionIcon, Box, ColorSwatch, Container, Group, NumberFormatter, Progress, SimpleGrid, Space, Stack, Text, Title } from "@mantine/core";
+import { ActionIcon, Box, ColorSwatch, Container, Divider, em, Grid, Group, NumberFormatter, Progress, SimpleGrid,  Stack, Text, Title } from "@mantine/core";
 import { useState } from "react";
 import styles from "./NetworthSection.module.css"
 import { IconPlus, IconSettingsAutomation } from "@tabler/icons-react";
+import { useMediaQuery } from "@mantine/hooks";
 
 const NetWorthSection = () => {
-    const [netWorthData, setNetWorthData] = useState([
-      {
-        id: 1,
-        value: 25900,
-        label: "Deposit",
-        color: "orange",
-        striped: true,
-      },
-      {
-        id: 2,
-        value: 30000,
-        label: "Savings",
-        color: "indigo",
-        striped: false,
-      },
-      {
-        id: 3,
-        value: 5900,
-        label: "Debts",
-        color: "violet",
-        striped: false,
-      },
-      {
-        id: 4,
-        value: 59000,
-        label: "Investment",
-        color: "pink",
-        striped: false,
-      },
-    ]);
-  
-    const addRandomNetWorthData = () => {
-      const newId = netWorthData.length + 1;
-      const newValue = Math.floor(Math.random() * 100000); // Random value between 0 and 100,000
-      const newLabel = `Random Label ${newId}`; // Example label, can be customized
-      const newColor = "#" + Math.floor(Math.random() * 16777215).toString(16); // Random hex color
-      const newStriped = Math.random() > 0.5;
-  
-      const newNetWorthItem = {
-        id: newId,
-        value: newValue,
-        label: newLabel,
-        color: newColor,
-        striped: newStriped,
-      };
-  
-      setNetWorthData((prevData) => [...prevData, newNetWorthItem]);
-    };
-  
-    const segments = netWorthData.map((segment) => {
-      return (
-        <Progress.Section
-          striped={segment.striped}
-          key={segment.id}
-          value={segment.value / 100}
-          label={segment.label}
-          color={segment.color}
-        />
-      );
-    });
-  
-    const swatches = netWorthData.map((swatch) => {
-      return (
-        <Group key={swatch.id} gap={"3px"}>
-          <ColorSwatch size={15} color={swatch.color} />
-          <Text fz={"sm"} fw={"normal"}>
-            {swatch.label}
-          </Text>
-          <Text c={"dimmed"} fw={"lighter"}>
-            <NumberFormatter prefix="$" value={swatch.value} thousandSeparator />
-          </Text>
-        </Group>
-      );
-    });
-  
-    return (
-      <Container mt={50} size={"lg"}>
-        <Stack gap={'xl'}>
-          <SimpleGrid cols={{ base: 1,xs:1, sm:2, md: 2 }}>
-        <Title tt={"capitalize"}  fz={{ base: 40, xs: "h1", sm: "h1", md: 40 }}>
-        Your networth <br /> visualized in real time
-        </Title>
-<Text>Track your net worth as it evolves in real-time. Get a clear, up-to-the-minute view of your assets, liabilities, and overall financial health, all in one place.</Text>
-          </SimpleGrid>
-        <Container w={'100%'} size={'md'}>
+  const [netWorthData, setNetWorthData] = useState([
+    {
+      id: 1,
+      value: 25900,
+      label: "Deposit",
+      color: "orange",
+      striped: true,
+    },
+    {
+      id: 2,
+      value: 30000,
+      label: "Savings",
+      color: "indigo",
+      striped: false,
+    },
+    {
+      id: 3,
+      value: 5900,
+      label: "Debts",
+      color: "violet",
+      striped: false,
+    },
+    {
+      id: 4,
+      value: 59000,
+      label: "Investment",
+      color: "pink",
+      striped: false,
+    },
+  ]);
 
+  const isMobile = useMediaQuery(`(max-width: ${em(768)})`);
+
+  const addRandomNetWorthData = () => {
+    const newId = netWorthData.length + 1;
+    const newValue = Math.floor(Math.random() * 100000); // Random value between 0 and 100,000
+    const newLabel = `Random Label ${newId}`; // Example label, can be customized
+    const newColor = "#" + Math.floor(Math.random() * 16777215).toString(16); // Random hex color
+    const newStriped = Math.random() > 0.5;
+
+    const newNetWorthItem = {
+      id: newId,
+      value: newValue,
+      label: newLabel,
+      color: newColor,
+      striped: newStriped,
+    };
+
+    setNetWorthData((prevData) => [...prevData, newNetWorthItem]);
+  };
+
+  const segments = netWorthData.map((segment) => {
+    return (
+      <Progress.Section
+        striped={segment.striped}
+        key={segment.id}
+        value={segment.value / 100}
+        label={segment.label}
+        color={segment.color}
+      />
+    );
+  });
+
+  const swatches = netWorthData.map((swatch) => {
+    return (
+      <Group key={swatch.id} gap={"3px"}>
+        <ColorSwatch size={15} color={swatch.color} />
+        <Text fz={"sm"} fw={"normal"}>
+          {swatch.label}
+        </Text>
+        <Text c={"dimmed"} fw={"lighter"}>
+          <NumberFormatter prefix="$" value={swatch.value} thousandSeparator />
+        </Text>
+      </Group>
+    );
+  });
+
+  return (
+    <Container mt={50} size={"lg"}>
+      <Stack gap={'xl'}>
+        <SimpleGrid cols={{ base: 1, xs: 1, sm: 2, md: 2 }}>
+          <Title tt={"capitalize"} fz={{ base: 40, xs: "h1", sm: "h1", md: 40 }}>
+            Your networth <br /> visualized in real time
+          </Title>
+          <Text c={'dimmed'}>Track your net worth as it evolves in real-time. Get a clear, up-to-the-minute view of your assets, liabilities, and overall financial health, all in one place.</Text>
+        </SimpleGrid>
+        <Container my={40} w={'100%'} size={'md'}>
           <div className={styles.networth_box_blur}>
-            
+
             <div className={styles.networth_content}>
               <Group gap={"xs"} justify="end">
                 <ActionIcon
@@ -153,9 +155,61 @@ const NetWorthSection = () => {
             </div>
           </div>
         </Container>
-        </Stack>
-      </Container>
-    );
-  };
-  
-  export default NetWorthSection
+        <Box>
+          <Divider color="var(--mantine-color-dark-7" />
+          <Grid m={"xs"}>
+            <Grid.Col h={400} span={{ base: 12, xs: 12, sm: 6, md: 6, lg: 6 }} style={{
+              paddingInlineEnd: !isMobile
+                ? "var(--mantine-spacing-xl)"
+                : null,
+            }}>
+              <Stack py={"xl"}>
+                <Box>
+                  <Title order={3} fw={"normal"}>
+                    Impact Analysis
+                  </Title>
+                  <Text c={"dimmed"}>
+                    Get break down how your spending, savings, and transactions directly affect your net worth, giving you a clear view of how daily decisions shape your financial future.
+                  </Text>
+                </Box>
+              </Stack>
+            </Grid.Col>
+            <Grid.Col
+              h={400}
+              span={{ base: 12, xs: 12, sm: 6, md: 6, lg: 6 }}
+              styles={{
+                col: {
+                  borderLeft: !isMobile
+                    ? `thin solid var(--mantine-color-dark-7)`
+                    : "none",
+                  borderTop: !isMobile
+                    ? "none"
+                    : `thin solid var(--mantine-color-dark-7)`,
+                  paddingInlineStart: !isMobile
+                    ? "var(--mantine-spacing-xl)"
+                    : null,
+                },
+              }}
+            >
+              <Stack py={"xl"}>
+                <Box>
+                  <Title order={3} fw={"normal"}>
+                    Net-Worth Drivers
+                  </Title>
+                  <Text c={"dimmed"}>
+                    Boost your account experience with a range of smart,
+                    easy-to-customize features .
+                  </Text>
+                </Box>
+              </Stack>
+            </Grid.Col>
+          </Grid>
+          <Divider color="var(--mantine-color-dark-7" />
+
+        </Box>
+      </Stack>
+    </Container>
+  );
+};
+
+export default NetWorthSection
