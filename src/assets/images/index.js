fd2959a8c54695fa1fd95dd780460a1533a2c155
catalyst <wealthralph@gmail.com>
2024-10-14 06:55:01 +0100

    updated send ui animation

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,13 @@
 import logoWhite from "./logo-white.png"
+import avatar1 from "./avatar_1.png"
+import avatar2 from "./avatar_2.jpg"
+import avatar3 from "./avatar_3.jpg"
+import avatar4 from "./avatar_4.png"
 
 export {
-    logoWhite
+    logoWhite,
+    avatar1,
+    avatar2,
+    avatar3,
+    avatar4
 }
