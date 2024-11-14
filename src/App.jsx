9ae199ebbca04854ba379a6bf43a310c15c241df
catalyst <wealthralph@gmail.com>
2024-11-14 22:34:30 +0100

    added posthog integration for web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
-import { Checkbox, createTheme, Divider, Input, MantineProvider, Paper } from "@mantine/core";
+import {
+  Checkbox,
+  createTheme,
+  Divider,
+  Input,
+  MantineProvider,
+  Paper,
+} from "@mantine/core";
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import WebLayout from "./components/layout/WebLayout.jsx";
 import EarlyAccess from "./pages/earlyAccess/EarlyAccess.jsx";
+import posthog from "posthog-js";
+import { useEffect } from "react";
 
 const theme = createTheme({
   primaryColor: "indigo",
   components: {
-    Paper: Paper.extend({
-    
-  
-    })
+    Paper: Paper.extend({}),
   },
   Divider: Divider.extend({
-    defaultProps:{
-      color: 'dark.7'
-    }
+    defaultProps: {
+      color: "dark.7",
+    },
   }),
-
 });
 
 const App = () => {
+
+  useEffect(() => {
+    posthog.init("phc_plHrpcMhzRVmnsI4YSLuN5kMOyIEInIx63gpDQrxz63", {
+      api_host: "https://us.i.posthog.com",
+      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    });
+    
+  }, []);
+
   return (
     <BrowserRouter>
       <MantineProvider
@@ -33,9 +47,8 @@ const App = () => {
           <Route path="/">
             <Route element={<WebLayout />}>
               <Route index element={<Home />} />
-
             </Route>
-              <Route path="early_access"  element={<EarlyAccess/>} />
+            <Route path="early_access" element={<EarlyAccess />} />
           </Route>
         </Routes>
       </MantineProvider>
